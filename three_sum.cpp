2897ec634f0f2ef1080d19ad6aee4d4b42c2380c
mrasquinha <mitch.rasquinha@gmail.com>
2015-09-01 22:26:12 +0300

    clean

--- three_sum.cpp
+++ three_sum.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <ctime>
 #include <assert.h>
+#include <strings.h>
 
 // Solve the 3sum problem: Given an array(S) find all unique pairs 
 // such that a+b=c and a,b,c belong to S and a != b != c
@@ -80,7 +81,7 @@ bool binary_search (int *elem, int lo, int hi, int search)
   return false;
 }
 
-void three_sum (int *elem, int N, bool has_neg) 
+void three_sum (int *elem, int N, bool has_neg)
 {
   int max = elem[N];
   int start = N;
@@ -118,6 +119,70 @@ void three_sum (int *elem, int N, bool has_neg)
   printf("\nthree_sum count = %d\n", is_3sum_cnt);
 }
 
+void three_sum_hash (int *elem, int N, bool has_neg)
+{
+  int max = elem[N-1];
+  int is_3sum_cnt = 0;
+  int *table = (int*)malloc(sizeof(int)*(max+1));
+  bzero(table, sizeof(int)*(max+1));
+printf("max is %d N is %d  ", max, N);
+
+  for (int a=0; a<N; a++) {
+    bool found = false;
+    for (int b=a+1; b<N; b++) {
+      int c = elem[a] + elem[b];
+      if(!found && table[elem[a]] != 0) { //was seen before
+        printf("\n pair (%d) ->%d counts ", elem[a], table[elem[a]]);
+        found = true;
+        is_3sum_cnt += table[elem[b]];
+      }
+      if(c>0 && c<=max) { 
+        printf("add (%d,%d,%d) ", elem[a], elem[b],c);
+        table[c]++;
+      }
+    }
+  }
+
+  printf("\nthree_sum count = %d\n", is_3sum_cnt);
+}
+
+//returns new length of the array
+// handle corner case of all 0's
+int remove_duplicates(int *elem, int N)
+{ 
+  int max_idx = 1;     //keep track of the last valid elem
+  bool is_dup = false; //optimize if array has no dup
+
+  // linear traversal of array; 0 out any duplicates
+  for (int i=1; i < N; i++) {
+    //move ahead till a dup is seen
+    while(elem[i]!=0 && elem[i] < elem[++i])
+      if(i == N) break;
+
+    if(elem[i-1]!=0 && elem[i-1]!=elem[max_idx])
+      max_idx = i-1;
+    else if (elem[i-1]!=0)
+      i--;
+
+    if (elem[i]!=0 && elem[i] == elem[max_idx]) {
+      //zero out copy
+      elem[i] = 0;
+      is_dup = true;
+    }
+  }
+
+  if(!is_dup) return N; //no duplicates found
+
+  // All duplicates are zero's at this point
+  max_idx =1; 
+  for (int i=1; i<N-1; i++) 
+    if(elem[i]==0 && elem[i+1]!=0) 
+      swap(&elem[max_idx++], &elem[i+1]);
+    
+  print_elem(elem, N, "remove dup");
+  return max_idx;
+}
+
 int main (int argc, char **argv) 
 {
   if(argc <= 1) {
@@ -146,12 +211,14 @@ int main (int argc, char **argv)
   clock_t startTime = clock();
   // sort the array using quick sort
   sort (elements, num_elem);
-  //print_elem(elements, num_elem, "Sorted");
-  three_sum (elements, num_elem-1, has_negative);
+  print_elem(elements, num_elem, "Sorted");
+  int new_size = remove_duplicates(elements, num_elem);
+
+  print_elem(elements, new_size, "New array");
+  three_sum_hash (elements, new_size, has_negative);
   clock_t endTime = clock();
 	clock_t time = (endTime - startTime)*1000.0/CLOCKS_PER_SEC;
 
   printf("Total time %0.2f ms\n", (double)time);
   exit(0);
 }
-
